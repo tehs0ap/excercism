91177e70cd6da83fabbe32d5819c42a1fa3df458
Martin Viens <marty.viens@gmail.com>
2025-04-17 15:49:10 -0400

    Completed C# annalyns-infiltration

---
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -0,0 +1,26 @@
+static class QuestLogic
+{
+    public static bool CanFastAttack(bool knightIsAwake)
+    {
+        return !knightIsAwake;
+    }
+
+    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
+    {
+        return  !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        if (petDogIsPresent && !archerIsAwake)
+        {
+            return true;
+        }
+        return !archerIsAwake && ! knightIsAwake && prisonerIsAwake;
+    }
+}
